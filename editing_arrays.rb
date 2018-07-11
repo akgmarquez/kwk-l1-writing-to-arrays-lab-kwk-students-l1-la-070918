a7ce4bd6f93f02ada318ec515421f9ed7e57fe69
akgmarquez <akgmarquez@gmail.com>
2018-07-11 18:23:55 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -13,7 +13,6 @@ change_rainbow_colors
 
 
 def add_colors
- RAINBOW_COLORS.push "green"
-  RAINBOW_COLORS "blue"
-  return RAINBOW_COLORS
+  RAINBOW_COLORS << "green"
+  RAINBOW_COLORS << "blue"
 end
