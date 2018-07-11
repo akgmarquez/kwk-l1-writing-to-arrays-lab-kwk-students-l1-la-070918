29056442007dafac023c981d799d5573b2fc0b39
akgmarquez <akgmarquez@gmail.com>
2018-07-11 18:14:14 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,9 +8,9 @@ def change_rainbow_colors
   rainbow_colors[0] = "red"
   puts rainbow_colors[0]
   rainbow_colors[1] = "light_red"
-puts rainbow_colors[1]
-rainbow_colors[2]="light_yellow"
-puts rainbow_colors[2]
+  puts rainbow_colors[1]
+  rainbow_colors[2]="light_yellow"
+  puts rainbow_colors[2]
 
 
 end
